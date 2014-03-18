93b8767d0b87a29417e8251b14da012ee4ebd0b6
Cory Koch <yunusabd786@gmail.com>
2014-03-18 10:25:50 -0400

    added check for source file

--- src/jaz/Interpreter.java
+++ src/jaz/Interpreter.java
@@ -1,7 +1,6 @@
 package jaz;
 
 public class Interpreter {
-
 	public static void main(String[] args) {
 		if (!(args.length > 0)) {
 			System.out.println("Must provide a jaz file");
@@ -15,3 +14,4 @@ public class Interpreter {
 
 
 
+   
